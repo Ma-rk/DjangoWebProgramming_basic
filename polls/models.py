01eb1f54799836eb679cd288bba89a7dd182d641
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-19 03:31:59 +0900

    added model: Answer

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,15 @@ class Question(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Answer(models.Model):
+    # region FIELDS
+    question_id = models.ForeignKey(Question)
+    answer_text = models.CharField(max_length=200)
+    num_of_votes = models.IntegerField(default=0)
+
+    # endregion
+
+    def __str__(self):
+        return self.answer_text
