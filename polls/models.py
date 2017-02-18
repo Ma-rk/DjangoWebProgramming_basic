9a643aa5f6c16edca177a6279618c889a68d6358
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-19 03:31:30 +0900

    added model: Question

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    # region FIELDS
+    text = models.CharField(max_length=200)
+    create_dt = models.DateTimeField('created date')
+
+    # endregion
+
+    def __str__(self):
+        return self.text
