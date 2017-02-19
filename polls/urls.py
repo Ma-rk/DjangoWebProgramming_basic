8306cabf3c45e70e010ecaa1cf05e84c4fdee626
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-19 19:48:30 +0900

    added url for detail(), detail.html

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,6 @@ from polls import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_selected_question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<user_selected_question_id>[0-9]+)/$', views.detail, name='vote'),
 ]
