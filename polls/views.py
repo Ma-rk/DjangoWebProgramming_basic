a5116c068b32548a8e316e0912df8a3cfeba7d3c
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-19 19:51:31 +0900

    add view function: detail()

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Question
 
 
 def index(request):
     question_list = Question.objects.all().order_by('-create_dt')
     return render(request, 'polls/index.html', {'question_list': question_list})
+
+
+def detail(request, user_selected_question_id):
+    question = get_object_or_404(Question, pk=user_selected_question_id)
+    return render(request, 'polls/detail.html', {'question': question})
