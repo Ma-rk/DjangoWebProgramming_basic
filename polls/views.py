5bcde6da5b6a712415edca1589b43797d2ea6532
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-19 20:18:29 +0900

    add view function: vote()

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from polls.models import Question
+from polls.models import Question, Answer
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 def index(request):
@@ -10,3 +12,18 @@ def index(request):
 def detail(request, user_selected_question_id):
     question = get_object_or_404(Question, pk=user_selected_question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, user_selected_question_id):
+    question = get_object_or_404(Question, pk=user_selected_question_id)
+    try:
+        user_selected_answer = question.answer_set.get(pk=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'err_msg': "You didn't select an answer."
+        })
+    else:
+        user_selected_answer.num_of_votes += 1
+        user_selected_answer.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
